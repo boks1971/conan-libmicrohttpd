bb634e29e5d5f3943715fd51b602bbdfc456b95a
Don Goodman-Wilson <don@slack-corp.com>
2016-10-28 17:25:08 -0700

    Fix paths

--- conanfile.py
+++ conanfile.py
@@ -43,7 +43,7 @@ class LibmicrohttpdConan(ConanFile):
             return
 
         if self.settings.os == "Linux" or self.settings.os == "Macos":
-            libs = 'LIBS="%s"' % " ".join(["-l%s" % lib for lib in self.deps_cpp_info.libs])
+            libs = 'LIBS="-liconv %s"' % " ".join(["-l%s" % lib for lib in self.deps_cpp_info.libs])
             ldflags = 'LDFLAGS="%s"' % " ".join(["-L%s" % lib for lib in self.deps_cpp_info.lib_paths]) 
             archflag = "-m32" if self.settings.arch == "x86" else ""
             cflags = 'CFLAGS="-fPIC %s %s"' % (archflag, " ".join(self.deps_cpp_info.cflags))
